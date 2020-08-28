bb4610faa05a7f1cc99f04c27ada12af7990b5bb
sp1ritCS <31540351+sp1ritCS@users.noreply.github.com>
2020-08-28 22:38:33 +0000

    Deploying to gh-pages from  @ a299217e189955ff8c734e0bb8e2c053635b3b5e 🚀

---
+++ precache-manifest.7ffca986747ba06b504f9dcdc098fcfa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f15ddecc2eecec94c6",
+    "url": "/css/about.faadb678.css"
+  },
+  {
+    "revision": "6ae831210e3281cdc698",
+    "url": "/css/app.32cd0ec5.css"
+  },
+  {
+    "revision": "172bc036209589f6cfd7",
+    "url": "/css/chunk-vendors.d599d135.css"
+  },
+  {
+    "revision": "8c286cde0eb18fbf4d78cc1c39d39f4a",
+    "url": "/img/diaspora.8c286cde.svg"
+  },
+  {
+    "revision": "d35e74d6c9c8eef31e37bf81af5b7b10",
+    "url": "/img/facebook.d35e74d6.svg"
+  },
+  {
+    "revision": "b3155900e94449481985b07b46660f71",
+    "url": "/img/hackernews.b3155900.svg"
+  },
+  {
+    "revision": "f3a96b044f1e78fdcb2dfde4460c5938",
+    "url": "/img/linkedin.f3a96b04.png"
+  },
+  {
+    "revision": "f88410de5343a95293089b0f27b5ddde",
+    "url": "/img/logo.f88410de.svg"
+  },
+  {
+    "revision": "e85aa8a265d50972cb5469f5dfac51ee",
+    "url": "/img/mastodon.e85aa8a2.png"
+  },
+  {
+    "revision": "65f609a318234caac066c7b46019c0fd",
+    "url": "/img/reddit.65f609a3.svg"
+  },
+  {
+    "revision": "c035a89abba91129573932bd2a98c327",
+    "url": "/img/sp1rit_scaled.c035a89a.svg"
+  },
+  {
+    "revision": "30374f00fb96e67005e46d7aadd043ea",
+    "url": "/img/twitter.30374f00.svg"
+  },
+  {
+    "revision": "3ef6d890c328ab906d7aada90bcd03bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f15ddecc2eecec94c6",
+    "url": "/js/about.0e5ce689.js"
+  },
+  {
+    "revision": "6ae831210e3281cdc698",
+    "url": "/js/app.23104c35.js"
+  },
+  {
+    "revision": "172bc036209589f6cfd7",
+    "url": "/js/chunk-vendors.d4a99cdf.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
